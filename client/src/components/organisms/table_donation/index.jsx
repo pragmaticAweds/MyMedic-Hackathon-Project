07e625608778d4f05991529a5e8d5d4eb26b8431
Yuuuusssuufff <ajibolayusuf8@gmail.com>
2023-01-25 22:50:47 +0100

    Donation Page

--- client/src/components/organisms/table_donation/index.jsx
+++ client/src/components/organisms/table_donation/index.jsx
@@ -1,38 +1,25 @@
 import clsx from "clsx";
 import { useState } from "react";
+import Button from "../../atoms/button";
 import PaginationArrowLeft from "../../atoms/vectors/PaginationArrowLeft";
 import PaginationArrowRight from "../../atoms/vectors/PaginationArrowRight";
-import "./TableStyles.scss";
+import "../table/TableStyles.scss";
 
-const Table = ({
-  tabSN,
-  gridStyle = "grid-cols-[0.25fr_0.25fr_0.15fr_0.15fr_0.15fr_0.15fr_0.1fr]",
+const TableDonation = ({
+  
   tableData,
-  tableDataHeading =  [
-    "Name",
-    "Email",
-    "Date",
-    "Visit Time",
-    "Doctor",
-    "Purpose",
-    "Status",
-  ],
+   tableDataHeading = ["S/N", "Name", "Email", "Date", "Donation", "Method", ""],
 }) => {
   const [currentPagination, setCurrentPagination] = useState(1);
-  const tableHeading = [
-    "Name",
-    "Email",
-    "Date",
-    "Visit Time",
-    "Doctor",
-    "Purpose",
-    "Status",
-  ];
 
   return (
     <div className={clsx("table-container")}>
       <div
-        className={clsx("table-head-container", "grid items-center", gridStyle)}
+        className={clsx(
+          "table-head-container",
+          "grid items-center",
+          "donationGridStyle"
+        )}
       >
         {tableDataHeading.map((th, i) => (
           <div key={`${th}-1 + ${i}`} className={clsx("table-head")}>
@@ -48,13 +35,13 @@ const Table = ({
               className={clsx(
                 "table-data-row",
                 "grid items-center",
-                gridStyle,
+                "donationGridStyle",
                 i === arr.length - 1 ? "border-y" : "border-t"
               )}
               key={"appointment-table-data" + i}
             >
-            
-                      
+              <span className="text-start">{i<9 ?`0${i + 1}`: i + 1}</span>
+
               <div className="flex items-center gap-x-5">
                 <div className="w-10 h-10 flex rounded-full">
                   <img
@@ -66,11 +53,12 @@ const Table = ({
                 <span>Yusuf Ajibola</span>
               </div>
               <span className="tb-email">Anonymous@gmail.com</span>
-              <span className="table-date ">10/10/20</span>
-              <span className="table-time">01:15-01:45pm</span>
-              <span className="table-time">Dr Ajisafe</span>
-              <span className="">Treatment</span>
-              <span className="">Waiting</span>
+              <span className="table-date ">Money</span>
+              <span className="table-date">25/01/2023</span>
+              <span className="table-time">Bank transfer</span>
+              <span className="">
+                <Button type="primary-btn" label="view" />
+              </span>
             </div>
           ))}
       </div>
@@ -113,4 +101,4 @@ const Table = ({
   );
 };
 
-export default Table;
+export default TableDonation;
--- client/src/pages/donations/index.jsx
+++ client/src/pages/donations/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Table from "../../components/organisms/table";
+import TableDonation from "../../components/organisms/table_donation";
 import ScreenLayout from "../../templates/screen-layout";
 
 const DonationsPage = () => {
-  return <ScreenLayout title="Donation"></ScreenLayout>;
+  const tableHeading = [ "S/N","Name", "Email", "Donation", "Date", "Method", "" ]
+  return <ScreenLayout title="Donations" inputName="filterAppointment"
+ >
+  <TableDonation tableDataHeading={tableHeading}/>
+    {/* <Table tableDataHeading={tableHeading}/> */}
+  </ScreenLayout>;
 };
 
 export default DonationsPage;
