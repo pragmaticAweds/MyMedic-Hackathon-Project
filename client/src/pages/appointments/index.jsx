08628bcf00e197c5d792bad74bec9400839195ea
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-01-26 21:42:41 +0100

    minor fixes

--- client/src/pages/appointments/index.jsx
+++ client/src/pages/appointments/index.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+import Modal from "../../components/molecules/Modal";
 import Table from "../../components/organisms/Table";
 import ScreenLayout from "../../templates/screen-layout";
 
 const AppointmentsPage = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  const handleModal = () => {
+    setShowModal((prev) => !prev);
+  };
   return (
     <ScreenLayout
       title="Appointment"
       btnLabel="Add Appointment"
       inputName="filterAppointment"
+      onClick={handleModal}
     >
       <Table />
+      <Modal handleCloseModal={handleModal} closeModal={showModal} />
     </ScreenLayout>
   );
 };
