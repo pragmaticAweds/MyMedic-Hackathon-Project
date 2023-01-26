c130690689600759b0d2c8c3de3be415443f2b86
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-01-26 20:02:24 +0100

    minor fixes

--- client/src/pages/overview/index.jsx
+++ client/src/pages/overview/index.jsx
@@ -44,50 +44,7 @@ const OverviewPage = () => {
       color: "#B40800",
     },
   ];
-  // const data = [
-  //   {
-  //     name: "Page A",
-  //     uv: 4000,
-  //     // pv: 2400,
-  //     // amt: 2400,
-  //   },
-  //   {
-  //     name: "Page B",
-  //     uv: 3000,
-  //     // pv: 1398,
-  //     // amt: 2210,
-  //   },
-  //   {
-  //     name: "Page C",
-  //     uv: 2000,
-  //     // pv: 9800,
-  //     // amt: 2290,
-  //   },
-  //   {
-  //     name: "Page D",
-  //     uv: 2780,
-  //     // pv: 3908,
-  //     // amt: 2000,
-  //   },
-  //   {
-  //     name: "Page E",
-  //     uv: 1890,
-  //     // pv: 4800,
-  //     // amt: 2181,
-  //   },
-  //   {
-  //     name: "Page F",
-  //     uv: 2390,
-  //     // pv: 3800,
-  //     // amt: 2500,
-  //   },
-  //   {
-  //     name: "Page G",
-  //     uv: 3490,
-  //     // pv: 4300,
-  //     // amt: 2100,
-  //   },
-  // ];
+
   const data = [
     {
       name: "2020-1",
@@ -142,7 +99,7 @@ const OverviewPage = () => {
   ];
   return (
     <DashboardLayout>
-      <div className="overview-container">
+      <div className="overview-container scrollbar-hide">
         <div className="flex gap-x-6">
           {dashBoard.map(({ color, label, icon, value }, i) => (
             <div
@@ -165,53 +122,58 @@ const OverviewPage = () => {
             </div>
           ))}
         </div>
-        <div className="w-full h-[25rem] bg-white rounded-lg py-6 px-2 flex flex-col gap-y-9">
+        <div className="w-full bg-white rounded-lg py-6 px-2 flex flex-col gap-y-9">
           <span className="font-bold text-xl text-[#333333] ml-6">
             Patient Survey
           </span>
-          <ResponsiveContainer width="100%" height="100%">
-            <AreaChart
-              width={500}
-              height={400}
-              data={data}
-              margin={{
-                // top: 0,
-                right: 30,
-                left: 0,
-                bottom: 0,
-              }}
-            >
-              <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#336CFB" stopOpacity={0.2} />
-                  <stop offset="95%" stopColor="#336CFB" stopOpacity={0} />
-                </linearGradient>
-                <linearGradient id="colorAv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#FAC032" stopOpacity={0} />
-                  <stop offset="5%" stopColor="#FAC032" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <CartesianGrid strokeDasharray="3 " strokeOpacity={0.5} />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Area
-                type="monotone"
-                dataKey="uv"
-                stroke="#336CFB"
-                fillOpacity={1}
-                fill="url(#colorUv)"
-              />
-              <Area
-                type="monotone"
-                dataKey="av"
-                stroke="#FAC032"
-                fill="url(#colorAv)"
-              />
-            </AreaChart>
-          </ResponsiveContainer>
+          <div className="h-[18rem]">
+            <ResponsiveContainer width="100%" height="100%">
+              <AreaChart
+                width={500}
+                height={400}
+                data={data}
+                margin={{
+                  top: 0,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
+                }}
+              >
+                <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#336CFB" stopOpacity={0.2} />
+                    <stop offset="95%" stopColor="#336CFB" stopOpacity={0} />
+                  </linearGradient>
+                  <linearGradient id="colorAv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#FAC032" stopOpacity={0} />
+                    <stop offset="5%" stopColor="#FAC032" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+                <CartesianGrid strokeDasharray="3 " strokeOpacity={0.5} />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Area
+                  type="monotone"
+                  dataKey="uv"
+                  stroke="#336CFB"
+                  fillOpacity={1}
+                  fill="url(#colorUv)"
+                />
+                <Area
+                  type="monotone"
+                  dataKey="av"
+                  stroke="#FAC032"
+                  fill="url(#colorAv)"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
         </div>
-        <div className="h-[30%]">
+        <div className="flex flex-col gap-y-4">
+          <span className="font-bold text-xl text-[#333333]">
+            Appointment Activity
+          </span>
           <Table />
         </div>
       </div>
